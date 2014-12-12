f993057cbedde93998821dd76967bdb89df8a2d1
Karthik Balakrishnan <torcellite@gmail.com>
2014-12-13 03:07:59 +0530

    Initial code commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 Android-Application-Translation
 ===============================
 
-Automation tool to translate strings.xml using Google Translate
+#####Project Synopsis 
+
+This is a rudimentary Google App Engine python application to translate the contents of `strings.xml`
+
+You can use the application here - [android-app-translation](http://android-app-translation.appspot.com)
+
+#####Example
+
+1. General translation
+
+        From: English
+        <strings name="monday">Monday</string>
+        To: German
+        <strings name="monday">Montag</string>
+        
+2. Exclude certain strings
+        From: English
+        <string name="monday"><!--exclude-->Monday</string>
+        To: German
+        <string name="monday"><!--exclude-->Monday</string>
+
+
+#####Things that need to be fixed/built
+1. A more fluid, sleek UI
+2. Simultaneous translations of multiple languages
+3. Direct downloads for the new strings.xml files
+
+#####Contributions
+
+If you'd like to contribute to the project, feel free to fork the project make your changes and commit it to a separate branch and then perform a pull request.
+
+#####Licensing
+The project is licensed under the MIT License.
+
+#####Screenshots
+
+![screenshot1](screenshot1.png)
+![screenshot2](screenshot2.png)
+![screenshot3](screenshot3.png)
---
+++ src/main.py
@@ -0,0 +1,93 @@
+import os
+import re
+import cgi
+import json
+import time
+import jinja2
+import urllib
+import urllib2
+import logging
+import webapp2
+import HTMLParser
+import lib.goslate as goslate
+
+from operator import itemgetter
+from collections import OrderedDict
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.pardir))),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+
+    def __init__(self, request, response):
+        self.initialize(request, response)
+
+    def get(self):
+        gs = goslate.Goslate()
+        languages = gs.get_languages()
+        languages = OrderedDict(sorted(languages.items(), key=itemgetter(1)))
+        template_values = {
+            'languages': languages
+        }
+        template = JINJA_ENVIRONMENT.get_template('static/index.html')
+        self.response.write(template.render(template_values))
+
+class Translator(webapp2.RequestHandler):
+
+    def __init__(self, request, response):
+        self.initialize(request, response)
+        self.to_lang = None
+        self.from_lang = None
+        self.translated = None
+
+    def post(self):
+        stringsxml = cgi.escape(self.request.get('stringsxml'))
+        self.to_lang = cgi.escape(self.request.get('languages'))
+        self.translated = self.translate(stringsxml)
+        template_values = {
+            'stringsxml': HTMLParser.HTMLParser().unescape(stringsxml),
+            'newstringsxml': HTMLParser.HTMLParser().unescape(self.translated),
+            'originallanguage': 'strings-' + self.from_lang + '.xml',
+            'translatedlanguage': 'strings-' + self.to_lang + '.xml'
+        }
+        template = JINJA_ENVIRONMENT.get_template('static/translate.html')
+        self.response.write(template.render(template_values))
+
+    def translate(self, stringsxml):
+        strings = stringsxml.split('\n')
+        pattern = '(&lt;.+&gt;)(.*|\\s*)(&lt;/.+&gt;)'
+        #Exclude strings with the comment <!--exclude--> between the <string> or <item> tags
+        exclude_pattern = '&lt;!--\s*exclude\s*--&gt;' 
+        gs = goslate.Goslate()
+        translate_this = []
+        for i in range(len(strings)):
+            match = re.search(pattern, strings[i])
+            if match:
+                translate_this.append(match.group(2))
+            else:
+                translate_this.append('')
+        translated = []
+        for i in gs.translate(translate_this, self.to_lang):
+            translated.append(i)
+        self.from_lang = gs.detect(translated[0])            
+        for i in range(len(strings)):
+            match = re.search(exclude_pattern, strings[i])
+            if match:
+              continue
+            else:
+              match = re.search(pattern, strings[i])
+              if match:
+                beginning = match.group(1)
+                end = match.group(3)
+                replacement = beginning + translated[i] + end
+                strings[i] = re.sub(pattern, replacement, strings[i])
+        newstringsxml = ''.join(strings)
+        return newstringsxml
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/translate', Translator),
+],debug=True)
